9593ba5c804bfedfa4e3cddf105e60ebbcb4c3dc
Amr <amr@eclipper.de>
2022-12-04 23:22:18 +0100

    add subscription and video history functionality

--- backend/src/main/java/com/amr/youtubeclone/controller/UserController.java
+++ backend/src/main/java/com/amr/youtubeclone/controller/UserController.java
@@ -3,12 +3,17 @@ package com.amr.youtubeclone.controller;
 
 
 import com.amr.youtubeclone.service.UserRegistrationService;
+import com.amr.youtubeclone.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/user")
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserRegistrationService userRegistrationService;
+    private final UserService userService;
 
     @GetMapping("/register")
     public String register(Authentication authentication){
@@ -23,5 +29,23 @@ public class UserController {
 
        userRegistrationService.registerUser(jwt.getTokenValue());
         return "User Registration successful";
+    }
+
+    @PostMapping("/subscribe/{userId}")
+    public boolean subscribeUser(@PathVariable String userId) {
+        userService.subscribeUser(userId);
+        return true;
+    }
+
+    @PostMapping("/unsubscribe/{userId}")
+    public boolean unSubscribeUser(@PathVariable String userId) {
+        userService.unSubscribeUser(userId);
+        return true;
+    }
+
+    @GetMapping("/{userId}/history")
+    @ResponseStatus(HttpStatus.OK)
+    public Set<String> userHistory(@PathVariable String userId) {
+        return userService.userHistory(userId);
     }
 }
--- backend/src/main/java/com/amr/youtubeclone/service/UserService.java
+++ backend/src/main/java/com/amr/youtubeclone/service/UserService.java
@@ -5,15 +5,22 @@ import com.amr.youtubeclone.model.User;
 import com.amr.youtubeclone.model.Video;
 import com.amr.youtubeclone.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    @Autowired
     UserRepository userRepository;
 
     public User getCurrentUser() {
@@ -59,5 +66,37 @@ public class UserService {
         User currentUser = getCurrentUser();
         currentUser.addToVideoHistory(videoId);
         userRepository.save(currentUser);
+    }
+
+    public void subscribeUser(String userId) {
+        User currentUser = getCurrentUser();
+        currentUser.addToSubscribedToUsers(userId);
+
+        User user = getUserById(userId);
+        user.addToSubscribers(currentUser.getId());
+
+        userRepository.save(currentUser);
+        userRepository.save(user);
+    }
+
+    public void unSubscribeUser(String userId) {
+        User currentUser = getCurrentUser();
+        currentUser.removeFromSubscribedToUsers(userId);
+
+        User user = getUserById(userId);
+        user.removeFromSubscribers(currentUser.getId());
+
+        userRepository.save(currentUser);
+        userRepository.save(user);
+    }
+
+    public Set<String> userHistory(String userId) {
+        User user = getUserById(userId);
+        return user.getVideoHistory();
+    }
+
+    private User getUserById(String userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Cannot fine user with userId - " + userId));
     }
 }
